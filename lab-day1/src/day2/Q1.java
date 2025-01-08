bb181cd79c091dee03378e9e2fbc625194dcccf7
Nitesh Yadav <nitesh.yadav@koerber-supplychain.com>
2025-01-08 11:54:53 +0530

    second commit

--- lab-day1/src/day2/Q1.java
+++ lab-day1/src/day2/Q1.java
@@ -1,31 +1,11 @@
 package day2;
 
-/*
-    Implement following UML diagram, Write a program to test circle class.
- */
-public class Employee{
-    private int id;
-    private String firstName;
-    private String lastName;
-    private double salary;
-}
-public  Employee(int id, String firstName, String lastName, double salary){
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.salary = salary;
-}
-
-
-
-
 public class Q1 {
-
-
-
     public static void main(String[] args) {
+        Q1Employee employee =new Q1Employee(1, "Nitesh", "Yadav", 5000);
+        System.out.println(employee);
+        double incrementedSalary = employee.raiseSalary(50);
+        System.out.println(incrementedSalary);
 
     }
-
-
 }
---
+++ lab-day1/src/day2/Q1Employee.java
@@ -0,0 +1,49 @@
+package day2;
+
+public class Q1Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+    public Q1Employee(int id, String firstName, String lastName, int salary){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public int getId(int id){
+        return id;
+    }
+    public String getFirstName(){
+        return lastName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public  String getName(){
+        String name = firstName + lastName;
+        return name;
+    }
+    public int getSalary(){
+        return salary;
+    }
+    public int setSalary(int salary){
+        return this.salary = salary;
+    }
+    public int getAnnualSalary(){
+        int toatalSalary = salary * 12;
+        return toatalSalary;
+    }
+    public int raiseSalary(int percentage){
+        double raiseAmount = salary * percentage/100;
+        return this.salary += raiseAmount;
+    }
+
+    public String toString(){
+        return "\n Employee Id is: " + id +
+                "\n Fistname is :" + firstName +
+                "\n Lastname is :" + lastName +
+                "\n Salary is : " + salary;
+    }
+}
